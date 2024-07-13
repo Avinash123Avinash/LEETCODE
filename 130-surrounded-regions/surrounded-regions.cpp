965ac7b01ad0f3aa8044582630c4373b20294413
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-07-13 20:00:36 +0530

    Time: 10 ms (65.07%) | Memory: 14.5 MB (19.36%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -4,8 +4,7 @@ public:
         int n = mat.size();
         int m = mat[0].size();
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<pair<int, int>> q;
-        vector<vector<char>> image = mat; // Initialize image directly from mat
+        queue<pair<int, int>>q;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -17,7 +16,6 @@ public:
                 }
             }
         }
-
         int delrow[4] = {-1, 0, +1, 0};
         int delcol[4] = {0, +1, 0, -1};
 
@@ -28,8 +26,8 @@ public:
             for (int i = 0; i < 4; i++) {
                 int nrow = r + delrow[i];
                 int ncol = c + delcol[i];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
-                    mat[nrow][ncol] == 'O' && vis[nrow][ncol] == 0) {
+                if (nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and
+                    mat[nrow][ncol] == 'O' and vis[nrow][ncol] == 0) {
                     q.push({nrow, ncol});
                     vis[nrow][ncol] = 1;
                 }
@@ -38,7 +36,7 @@ public:
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 'O' && vis[i][j] == 0) {
+                if (mat[i][j] == 'O' and vis[i][j] == 0) {
                     mat[i][j] = 'X';
                 }
             }
