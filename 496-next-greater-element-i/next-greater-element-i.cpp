95c2f545bb64b029722cf393168da45f7dea55d0
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-08-03 01:19:49 +0530

    Time: 4 ms (70.28%) | Memory: 12.7 MB (12.66%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,37 +3,21 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
        int n=nums1.size();
        int m=nums2.size();
-    //    if(m==1)
-    //    return {-1};
-       vector<int>index;
-       for(int i=0;i<n;i++)
-       {
-        for(int j=0;j<m;j++)
+    stack<int>s;
+    unordered_map<int,int>mpp;
+    for(int i=0;i<m;i++)
+    {
+        while(!s.empty() and s.top()<nums2[i])
         {
-            if(nums1[i]==nums2[j])
-            index.push_back(j);
+          mpp[s.top()]=nums2[i];
+          s.pop();
         }
-       }
-       vector<int>ans;
-       int flag;
-       for(int i=0;i<n;i++)
-       {
-        int k=index[i];
-        for(int j=k;j<m;j++)
-        {
-            flag=0;
-            if(k==m)
-            ans.push_back(-1);
-            else if(nums2[k]<nums2[j])
-            {
-                ans.push_back(nums2[j]);
-                flag=1;
-                break;
-            }
-        }
-        if(!flag)
-        ans.push_back(-1);
-       } 
-       return ans;
+        s.push(nums2[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        nums1[i]=mpp[nums1[i]]?mpp[nums1[i]]:-1;
+    }
+    return nums1;
     }
 };
