2af921b2782349d5d82200a1c120b990854f0022
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-08-08 21:43:02 +0530

    Time: 11 ms (54.89%) | Memory: 13.5 MB (79.86%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -15,7 +15,7 @@ public:
         }
         for(int i=1;i<n+1;i++)
         {
-            for(int j=0;j<amount+1;j++)
+            for(int j=1;j<amount+1;j++)
             {
                 if(coins[i-1]<=j)
                 dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
