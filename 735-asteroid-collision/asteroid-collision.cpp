c4b3750b5c68dd35180648ef0b46a948e80f7d5d
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-08-11 03:40:58 +0530

    Time: 8 ms (74.00%) | Memory: 20.9 MB (29.70%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& nums) {
-        stack<int> st;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int asteroid = nums[i];
-            bool collision = false;
-            
-            while (!st.empty() && asteroid < 0 && st.top() > 0) {
-                // Check if top of stack and current asteroid will collide
-                if (st.top() < -asteroid) {
-                    st.pop();  // Top of stack explodes
-                    continue;  // Continue to check for more collisions
-                } else if (st.top() == -asteroid) {
-                    st.pop();  // Both explode
-                    collision = true;
-                    break;
-                } else {
-                    collision = true;
-                    break;  // Current asteroid explodes
-                }
+        int n=nums.size();
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+            int asteroid=nums[i];
+            bool colision=false;
+            while(!st.empty() and asteroid<0 and st.top()>0)
+            {
+               if(st.top()>-asteroid)
+               {
+                  colision=true;
+                  break;
+               }
+               else if(st.top()<-asteroid)
+               {
+                     st.pop();
+                     continue;
+               }
+               else
+               {
+                st.pop();
+                colision=true;
+                break;
+               }
             }
-            
-            if (!collision) {
-                st.push(asteroid);  // No collision, asteroid survives
-            }
+            if(!colision)
+            st.push(asteroid);
         }
-        
-        vector<int> ans;
-        while (!st.empty()) {
+    
+        vector<int>ans;
+        while(!st.empty())
+        {
             ans.push_back(st.top());
             st.pop();
         }
         
-        reverse(ans.begin(), ans.end());  // Reverse to get the correct order
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
