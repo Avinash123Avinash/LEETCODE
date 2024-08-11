9697eee2cfb8ed662e83ae4910f4a2a6332554f0
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-08-11 11:29:05 +0530

    Time: 139 ms (88.39%) | Memory: 106.5 MB (58.60%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool asteroidsDestroyed(long long mass, vector<int>& asteroid) {
-        sort(asteroid.begin(),asteroid.end());
-        long long mass1=mass;
-        int n=asteroid.size();
-        for(int i=0;i<n;i++)
-        {
-            if(mass1>=asteroid[i])
-            mass1=mass1+asteroid[i];
+        sort(asteroid.begin(), asteroid.end());
+        long long mass1 = mass;
+        int n = asteroid.size();
+        for (int i = 0; i < n; i++) {
+            if (mass1 >= asteroid[i])
+                mass1 = mass1 + asteroid[i];
             else
-            return false;
+                return false;
         }
-        if(mass1>asteroid[n-1])
+
         return true;
-        else
-        return false;
     }
 };
