f1f3ce2995849527cf0d3d9e9ef329a957da7a0c
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-11-07 23:59:26 +0530

    Time: 1 ms (45.15%) | Memory: 31.5 MB (25.68%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -1,26 +1,22 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        int n = num.size();
-        vector<int> ans;
-        int i = n - 1;
-        int carry = 0;
-
-        // Process each digit from the end of the array
-        while (i >= 0 || k > 0) {
-            if (i >= 0) {
-                k += num[i];  // Add digit from num to k
+        int n=num.size();
+        int i=n-1;
+        vector<int>ans;
+        while(i>=0 || k>0)
+        {
+            if(i>=0)
+            {
+                k+=num[i];
                 i--;
             }
-            ans.push_back(k % 10); // Take last digit of k
-            k /= 10; // Reduce k
-        }
+            
+            ans.push_back(k%10);
+            k/=10;
 
-        reverse(ans.begin(), ans.end()); // Reverse to get final result
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
