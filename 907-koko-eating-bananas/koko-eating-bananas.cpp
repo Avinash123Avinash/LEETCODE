030628407a5763467482948c0e27cb1925ad4d52
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-11-23 20:50:43 +0530

    Time: 19 ms (14.71%) | Memory: 22.5 MB (13.31%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        sort(piles.begin(),piles.end());
+        int f=1;
+        int l=piles[n-1];
+        long long total;
+          int ans=0;
+        while(f<=l)
+        {
+            int mid=(f+l)/2;
+             total=0;
+            for(int j=0;j<n;j++)
+            {
+                if(piles[j]%mid!=0)
+                total+=piles[j]/mid+1;
+                else
+                total+=piles[j]/mid;
+            }
+            if(total<=h)
+            {
+               ans=mid;
+               l=mid-1;
+            }
+            else
+            {
+                f=mid+1;
+            }
+
+
+        }
+        return ans;
+    }
+};
