6e32fda360f5969134bbd347e5a45d70afdf1049
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-12-15 21:11:44 +0530

    Time: 4 ms (25.72%) | Memory: 95 MB (56.40%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        int i=0;
-        int j=0;
-        while(i<nums1.size() and j<nums2.size())
-        {
-            if(nums1[i]<nums2[j])ans.push_back(nums1[i++]);
-            else
-            ans.push_back(nums2[j++]);
-        }
-        while(i<nums1.size())
-        {
-            ans.push_back(nums1[i++]);
-        }
-         while(j<nums2.size())
-        {
-            ans.push_back(nums2[j++]);
-        }
-        int n=ans.size();
-        double ans1=0;
-        // int f=0;
-        int l=n-1;
+       int n1=nums1.size();
+       int n2=nums2.size();
+       int n=n1+n2;
+       int ind2=n/2;
+       int ind1=ind2-1;
+       int cnt=0;
+       int i=0;
+       int j=0;
+       int ind1el=-1;
+       int ind2el=-1;
+       while(i<n1 and j<n2)
+{
+    if(nums1[i]<nums2[j])
+    {
+        if(cnt==ind1)ind1el=nums1[i];
+        if(cnt==ind2)ind2el=nums1[i];
+        cnt++;
+        i++;
+    }
+    else
+    {
+          if(cnt==ind1)ind1el=nums2[j];
+        if(cnt==ind2)ind2el=nums2[j];
+        cnt++;
+        j++;
+    }
+}
+while(i<n1)
+{
+     if(cnt==ind1)ind1el=nums1[i];
+        if(cnt==ind2)ind2el=nums1[i];
+        cnt++;
+        i++;
+}
+while(j<n2)
+{
+      if(cnt==ind1)ind1el=nums2[j];
+        if(cnt==ind2)ind2el=nums2[j];
+        cnt++;
+        j++;
+}
         if(n%2!=0)
         {
-            return (ans[l/2]);
+            return ind2el;
         }
         else
         {
-            return (double(ans[l/2]+ans[(l/2)+1])/2);
+            return ((double(ind1el)+double(ind2el))/2);
         }
     }
 };
