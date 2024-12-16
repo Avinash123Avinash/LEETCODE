135f01492a5ba3b7e932504ab1ec974a1e1b5e2e
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-12-17 02:19:06 +0530

    Time: 18 ms (17.21%) | Memory: 34.7 MB (38.64%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool possible(vector<int>& weights, int days, int num) {
+        int sum = 0;
+        int sumt = 0;
+        int cnt = 1;
+        // int j = 0;
+        for (int i = 0; i < weights.size(); i++) {
+           
+            if (sum+weights[i] > num) {
+                cnt++;
+                sum = 0;
+            }
+            sum += weights[i];
+        }
+        if (cnt <= days)
+            return true;
+        else
+            return false;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        int total=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+          total+=weights[i];
+          maxi=max(maxi,weights[i]);
+        }
+        int mini = INT_MAX;
+        int low=maxi;
+        int high=total;
+       while(low<=high) {
+        int mid=(low+high)/2;
+              
+            if (possible(weights,days,mid))
+                high=mid-1;
+                else
+                low=mid+1;
+        }
+        return low;
+    }
+};
