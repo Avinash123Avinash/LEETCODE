0c5f9775b9841028cb8e26051874a644677d5c47
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-12-24 00:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (59.54%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    void find(vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans, vector<bool>& used) {
-        // Base case: if the current permutation (temp) is complete
+    void find(vector<int>& nums,vector<int>& temp,   vector<vector<int>> &ans,vector<bool>&used) {
         if (temp.size() == nums.size()) {
             ans.push_back(temp);
             return;
         }
-
-        // Recursive backtracking
-        for (int i = 0; i < nums.size(); i++) {
-            if (!used[i]) { // Check if the current number is already used
+        for(int i=0;i<nums.size();i++)
+        {
+            if(!used[i])
+            {
                 temp.push_back(nums[i]);
-                used[i] = true; // Mark the number as used
-                find(nums, temp, ans, used); // Recurse for the next number
-                used[i] = false; // Backtrack
-                temp.pop_back(); // Remove the last number
+                used[i]=true;
+                find(nums,temp,ans,used);
+                used[i]=false;
+                temp.pop_back();
             }
         }
+
+
     }
-
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> temp; // Current permutation
-        vector<vector<int>> ans; // Result to store all permutations
-        vector<bool> used(nums.size(), false); // To track used elements
-
-        find(nums, temp, ans, used); // Start backtracking
+        int n = nums.size();
+        vector<int> temp;
+        vector<vector<int>> ans;
+       vector<bool>used(n,false);
+                find(nums, temp, ans,used);
+       
         return ans;
     }
 };
