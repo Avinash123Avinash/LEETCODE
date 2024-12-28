4b49e1190fc43b5ec444e72798efb05fed7b61d0
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-12-28 22:59:40 +0530

    Time: 1 ms (77.78%) | Memory: 11.3 MB (42.52%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int n = num.size();
+        string ans;
+        if (n == k)
+            return "0";
+    for(char dig:num)
+    {
+        while(!ans.empty() and k>0 and ans.back()>dig)
+        {
+            ans.pop_back();
+            k--;
+        }
+        ans+=dig;
+    }
+    while(k>0)
+    {
+        ans.pop_back();
+        k--;
+    }
+    // for check the leading zero
+    int i=0;
+    while(ans[i]=='0')
+    {
+        i++;
+    }
+   ans=ans.substr(i);
+    return ans.empty()?"0":ans;
+    }
+};
