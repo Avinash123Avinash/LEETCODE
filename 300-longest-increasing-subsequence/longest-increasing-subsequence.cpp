679591754cec35ff1ae1a7f247221329f43af928
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2024-12-31 14:45:29 +0530

    Time: 63 ms (60.29%) | Memory: 14.3 MB (47.22%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int ind, int prev, int n,
-          vector<vector<int>>& dp) {
+    // int f(vector<int>& nums, int ind, int prev, int n,
+    //       vector<vector<int>>& dp) {
 
-        if (ind == n)
-            return 0;
-        if (dp[ind][prev + 1] != -1)
-            return dp[ind][prev + 1];
-        // not take
-        int len1 = 0 + f(nums, ind + 1, prev, n, dp);
-        int len2 = 0;
-        if (prev == -1 || nums[ind] > nums[prev]) {
-            len2 = (1 + f(nums, ind + 1, ind, n, dp));
-        }
-        return dp[ind][prev + 1] = max(len1, len2);
-    }
+    //     if (ind == n)
+    //         return 0;
+    //     if (dp[ind][prev + 1] != -1)
+    //         return dp[ind][prev + 1];
+    //     // not take
+    //     int len1 = 0 + f(nums, ind + 1, prev, n, dp);
+    //     int len2 = 0;
+    //     if (prev == -1 || nums[ind] > nums[prev]) {
+    //         len2 = (1 + f(nums, ind + 1, ind, n, dp));
+    //     }
+    //     return dp[ind][prev + 1] = max(len1, len2);
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         // int len=0;
-        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int prev = ind - 1; prev >= -1; prev--) {
-                int len = 0 + dp[ind + 1][prev + 1];
-
-                if (prev == -1 || nums[ind] > nums[prev]) {
-                    len = max(len, (1 + dp[ind + 1][ind + 1]));
-                }
-                dp[ind][prev + 1] = len;
-            }
+        int maxi=INT_MIN;
+     vector<int>dp(n,1);
+     for(int ind=0;ind<n;ind++)
+     {
+        for(int prev=0;prev<ind;prev++)
+        {
+            if(nums[ind]>nums[prev])
+            dp[ind]=max(dp[prev]+1,dp[ind]);
         }
-
-        return dp[0][-1 + 1];
+        maxi=max(maxi,dp[ind]);
+     }
+     return maxi;
     }
 };
