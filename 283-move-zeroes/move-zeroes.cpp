d3eda5bce1fa198439719237d8979fee45a75484
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-08 23:25:03 +0530

    Time: 1 ms (21.96%) | Memory: 24.1 MB (7.71%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        vector<int>ans;
         int n=nums.size();
         int cnt=0;
-     queue<int>q;
-     for(int i=0;i<n;i++)
-     {
-        if(nums[i]!=0)
+        for(int i=0;i<n;i++)
         {
-            q.push(nums[i]);
-        }
-        else
-        {
+           if(nums[i]==0)
+           ans.push_back(0);
+           else
+           {
+            nums[cnt]=nums[i];
             cnt++;
-            continue;
+           }
         }
-     }
-     nums.clear();
-     while(!q.empty())
-     {
-        nums.push_back(q.front());
-        q.pop();
-     }
-        for(int i=n-cnt;i<n;i++)
+        int m=ans.size();
+        for(int i=n-m;i<n;i++)
         {
-            nums.push_back(0);
+            nums[i]=0;
         }
     }
 };
