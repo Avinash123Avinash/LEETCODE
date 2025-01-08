afd9a3c76c8a32ae95076cea30638a6864e768d1
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-08 13:33:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (5.15%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,25 +2,35 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n=nums.size();
-        vector<int>ans(n,0);
-        int k=0;
-        for(int i=1;i<n;i++)
-        {
-           if(nums[i]<nums[i-1])
-           {
-             k=i;
-            break;
-           }
-        }
+        vector<int>ans1(nums.begin(),nums.end());
+        sort(ans1.begin(),ans1.end());
+        // int i=0;
+        // int j=n-1;
+        // for(int i=0;i<n;i++)
+        // {
+        //     j=n-1;
+        //     while(i<j)
+        //     {
+        //         if(nums[i]>nums[j])
+        //         swap(nums[i],nums[j]);
+        //         j--;
+        //     }
+           
+        // }
+        //  if(nums==ans)
+        //     return true;
+        // return false;
+        vector<int>ans(n);
         for(int i=0;i<n;i++)
         {
-            ans[i]=nums[(i+k)%n];
+            // ans=nums;
+            for(int j=0;j<n;j++)
+            {
+                ans[j]=nums[(j+i)%n];
+            }
+           if(ans1==ans)
+           return true;
         }
-        for(int i=1;i<n;i++)
-        {
-            if(ans[i]<ans[i-1])
-            return false;
-        }
-        return true;
+        return false;
     }
 };
