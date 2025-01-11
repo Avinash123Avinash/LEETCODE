00876d8e1babb46842f8bd310e00738718fe0112
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-11 17:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (51.76%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,16 +1,42 @@
 class Solution {
 public:
+// here is teh bruete force approach
+    // void sortColors(vector<int>& nums) {
+    //     int n=nums.size();
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         int j=n-1;
+    //         while(i<j)
+    //         {
+    //             if(nums[i]>nums[j])
+    //             swap(nums[i],nums[j]);
+    //             j--;
+    //         }
+    //     }
+    // optimization to sort the array which contain only three type of distinct element
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            int j=n-1;
-            while(i<j)
+         int n=nums.size();
+         int l=0,m=0,h=n-1;
+         while(m<=h)
+         {
+            // here if we get the value 0 then swap with the nums[l] 
+            // for put 0 to the left side of the l pointer
+            if(nums[m]==0)
             {
-                if(nums[i]>nums[j])
-                swap(nums[i],nums[j]);
-                j--;
+                swap(nums[m],nums[l]);
+                l++,m++;
             }
-        }
+             else if(nums[m]==2)
+            {
+                swap(nums[m],nums[h]);
+                h--;
+            }
+            else
+            {
+                m++;
+            }
+
+         }
+
     }
 };
