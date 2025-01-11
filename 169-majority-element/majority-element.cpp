2938b772cb9535ddceb8f63edd7f4fbf5bb13aea
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-11 18:47:46 +0530

    Time: 5 ms (16.51%) | Memory: 28.2 MB (48.71%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
+    // int majorityElement(vector<int>& nums) {
+    //     int n=nums.size();
+    //     // int k=n/2;
+    //     map<int,int>mpp;
+    //     // for this loop the TC be O(nlog(n))
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         mpp[nums[i]]++;
+    //     }
+    //      // for this loop the TC be O(nlog(n))
+    //     for(auto it:mpp)
+    //     {
+    //         if(it.second>n/2)
+    //         return it.first;
+    //     }
+    //     return -1;
+int majorityElement(vector<int>& nums) {
+    sort(nums.begin(),nums.end());
         int n=nums.size();
-        int k=n/2;
-        map<int,int>mpp;
-        for(int i=0;i<n;i++)
+        int cnt=0;
+        for(int i=1;i<n;i++)
         {
-            mpp[nums[i]]++;
+            if(nums[i-1]==nums[i])
+            {
+                cnt++;
+            }
+            else
+            {
+                if(cnt+1>n/2)
+                return nums[i-1];
+                else
+                cnt=0;
+            }
         }
-        for(auto it:mpp)
-        {
-            if(it.second>k)
-            return it.first;
-        }
+        if(cnt+1>n/2)
+        return nums[n-1];
         return -1;
-    }
+}
+
+    
 };
