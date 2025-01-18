abb3304978a8c36dbd962474f878a01abaf8552a
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-18 12:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (32.24%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
 int MOD=1e9+7;
+// ki jab bhi ham  y ko divide karte hai tab agar odd rahata hai to ham ek number ko kam karlete hai iske liye agar y ka value odd hota hai tab ham ek x extra multiply karte hai
+// i.e 5/2=2 yaha par 3 part gaya lekin ham do ko consider kiye iske liye ham ek extar x multiply karte hai
 long long pow(long long x,long long y)
 {
     if(y==0)
-    return 1;
+    return 1.00000;
      long long ans=pow(x,y/2);
      ans*=ans;
     ans%=MOD;
