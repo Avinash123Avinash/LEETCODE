361b9a01379242ac9f11cb9f3b1a656d2bfee0f9
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-18 12:38:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (39.89%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
-void f(double& x,int n,double& ans)
+
+double solve(double x,int n)
 {
     if(n==0)
-    return;
-    if(n>0)
-    {
-        ans=ans*x;
-        f(x,n-1,ans);
-    }
-    else if(n<0)
-    {
-        ans=ans/x;
-        f(x,n+1,ans);
-    }
+    return 1.00000;
+
+    double ans=solve(x,n/2);
+    ans*=ans;
+    if(n%2)
+    ans*=x;
+    return ans;
 }
-    double myPow(double x, int n) {
-        //   if(x==1 || x==-1)
-        //   return x;
-        //   if(n==INT_MIN)
-        //   return 0;
-        //   double ans=x;
-        // f(x,n,ans);
-        // return ans/x;
-        double ans;
-        if(n<0) ans = 1.000/solve(x,-1*(long)n);
-        else ans = solve(x,n);
-
-        return ans;
-    }
-
-
-    double solve(double x, long n){
-        if(n==0)    return 1.000;
-        if(n==1)    return x;
-        double ans = solve(x,n/2);
-        if(n%2==0){
-            return ans*ans;
-        }else{
-            return ans*ans*x;
-        }
+ double myPow(double x, int n) {
+    double ans;
+    if(n<0) ans=1.00000/solve(x,n);
+    else
+    ans=solve(x,n);
+    return ans;
     }
 };
