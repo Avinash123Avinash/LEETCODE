2f26199653ce7c69c9147dc03270f6ab9c7c997e
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-20 11:54:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (77.07%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    bool rotateString(string s1, string s2) {
-        int n1=s1.length();
-        int n2 = s2.length();
-        if(n1!=n2)
-        return false;
-        for(int i=0;i<n2;i++)
+    bool rotateString(string s, string goal) {
+        int n=goal.size();
+        string ans(s.begin(),s.end());
+        for(int i=0;i<n;i++)
         {
-            if(s1.substr(i,n2)==s2)
-            return true;
-            s1+=s1[i];
+            for(int j=0;j<n;j++)
+            {
+            ans[j]=s[(i+j)%n];
+            }
+            if(ans==goal)
+            return 1;
         }
-        return false;
+        return 0;
+       
     }
 };
