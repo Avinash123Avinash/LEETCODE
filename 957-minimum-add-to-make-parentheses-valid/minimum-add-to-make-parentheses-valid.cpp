98bf4b99ae2ec0cc204c5303baa11ab0388a1fe8
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-29 02:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (55.29%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char> st;
-        st.push(s[0]);
-        int flag = 0;
-        for (int i = 1; i < s.size(); i++) {
-            if (st.size()!=0 and s[i] == ')' and st.top()=='(') {
-                st.pop();
-                // flag = 0;
+        // stack<char> st;
+        // st.push(s[0]);
+        // for (int i = 1; i < s.size(); i++) {
+        //     here we check that the top of the stack has '(' and s[i] is ')' then pop
+        //     // occured otherwise not produce the valid parenthesis
+        //     if (st.size()!=0 and s[i] == ')' and st.top()=='(') {
+        //         st.pop();
+        //     }
+        //      else {
+        //         st.push(s[i]);
+        //     }
+        // }
+        // return st.size();
+        int open=0;
+        int minadd=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+            open++;
             }
-             else {
-                
-                st.push(s[i]);
+            else
+            {
+                // here if open exist then it cancel with the close 
+                // and if not exist then add to required brackets
+                open>0?open--:minadd++;
             }
         }
-        return st.size();
+        return open+minadd;
     }
 };
