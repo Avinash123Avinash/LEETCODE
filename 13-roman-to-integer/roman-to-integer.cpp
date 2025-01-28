21737688ee21d9f0c3a50303662c0c3d2943e937
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-28 22:25:45 +0530

    Time: 7 ms (52.82%) | Memory: 13.3 MB (45.36%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -3,27 +3,25 @@ public:
     int romanToInt(string s) {
         int n = s.size();
         int sum = 0;
-        int num = 0;
+        // here we can allot all values to the map
       map<char, int> mpp{
         {'I', 1}, {'V', 5}, {'X', 10},
         {'L', 50}, {'C', 100}, {'D', 500},
         {'M', 1000}
     };
-    if(n==1)
-    return mpp[s[0]];
-        for (int i = 1; i < n; i++) {
-            if (mpp[s[i-1]] <mpp[s[i]] ) {
-                num = mpp[s[i]] - mpp[s[i - 1]];
-                sum += num;
-                i++;
-            } 
-            else {
-                sum += mpp[s[i - 1]];
-            }
-        }
-        if(mpp[s[n-2]]>=mpp[s[n-1]])
+    // here we check the base case for the single element
+    
+        for(int i=0;i<n;i++)
         {
-            sum+=mpp[s[n-1]];
+            // here at each cosecutive step check the element
+            if(i+1<n and mpp[s[i]]<mpp[s[i+1]])
+            {
+                sum-=mpp[s[i]];
+            }
+            else
+            {
+                sum+=mpp[s[i]];
+            }
         }
         return sum;
     }
