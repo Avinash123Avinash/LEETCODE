c4c227d0a076f8ce4c817a5b0b95c84760c1dad7
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-01-29 14:18:38 +0530

    Time: 235 ms (28.83%) | Memory: 29.1 MB (16.29%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,57 +1,56 @@
 class Solution {
 public:
     int beautySum(string s) {
-        // vector<string> ans;
-        // int n = s.size();
-        // int sum = 0;
-        // for (int i = 0; i < n; i++) {
-        //    string temp;
-        //     for (int j = i ; j <n; j++) {
-        //         temp += s[j];
-        //         ans.push_back(temp);    
-        //     }
+        vector<string> ans;
+        int n = s.size();
+        int sum = 0;
+        // for(int i=0;i<ans.size();i++)
+        // {
+        //     cout<<ans[i]<<endl;
         // }
-        
-        
-        // map<char, int> mpp;
-        // for (int i = 0; i < ans.size(); i++) {
-        //     int maxi = INT_MIN;
-        //     int mini = INT_MAX;
-        //     for (int j = 0; j < ans[i].size(); j++) {
-        //         mpp[ans[i][j]]++;
-        //         maxi = max(maxi, mpp[ans[i][j]]);
-        //         mini = min(mini, mpp[ans[i][j]]);
-        //     }
-        //     if (maxi != mini and mpp.size()>1) {
-        //         sum += (maxi - mini);
-        //     }
-        //     mpp.clear();
-        // }
-        // return sum;
-        int n=s.size();
-        int sum=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            vector<int>freq(26,0);
-            for(int j=i;j<n;j++)
-            {
-                freq[s[j]-'a']++;
-                  int maxi=INT_MIN;
-            int mini=INT_MAX;
-                for(int k=0;k<26;k++)
-                {
-                    if(freq[k]>0)
-                   
-                    {
-                        maxi=max(maxi,freq[k]);
-                        mini=min(mini,freq[k]);
-                    }
+
+        for (int i = 0; i < n; i++) {
+            map<char, int> mpp;
+            for (int j = i; j < n; j++) {
+
+                mpp[s[j]]++;
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
+                for (auto it : mpp) {
+                    maxi = max(maxi, it.second);
+                    mini = min(mini, it.second);
                 }
-                sum+=maxi-mini;
+
+                sum += (maxi - mini);
             }
         }
         return sum;
-
     }
+
+    //     int n=s.size();
+    //     int sum=0;
+
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         vector<int>freq(26,0);
+    //         for(int j=i;j<n;j++)
+    //         {
+    //             freq[s[j]-'a']++;
+    //               int maxi=INT_MIN;
+    //         int mini=INT_MAX;
+    //             for(int k=0;k<26;k++)
+    //             {
+    //                 if(freq[k]>0)
+
+    //                 {
+    //                     maxi=max(maxi,freq[k]);
+    //                     mini=min(mini,freq[k]);
+    //                 }
+    //             }
+    //             sum+=maxi-mini;
+    //         }
+    //     }
+    //     return sum;
+
+    // }
 };
