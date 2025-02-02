889da5c90156a2523c0318b216fe61af665a2f3e
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-02-03 02:32:27 +0530

    Time: 165 ms (5.10%) | Memory: 97.4 MB (9.78%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
-    int number(vector<int>& nums, int k) {
-        int n=nums.size();
-        int i=0;
-        int j=0;
-        int cnt=0;
-        int ans=0;
-        while(j<n)
+// // here we also use the concept of the variable sliding window
+//     int number(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         int i=0;
+//         int j=0;
+//         int cnt=0;
+//         int ans=0;
+//         // take two points and one less than n
+//         while(j<n)
+//         {
+//             // here the calculation part
+//           if(nums[j]%2!=0)
+//           k--;
+//         //   here the condition part which is used to eliminate the element
+//          while(i<=j and k<0)
+//          {
+//             if(nums[i]%2!=0)
+//             k++;
+//             i++;
+//          }
+//         //  here we cnt all the subarray which satisfies the condition
+//          cnt+=j-i+1;
+//          j++;
+//     }
+//     return cnt;
+//     }
+//      int numberOfSubarrays(vector<int>& nums, int k){
+//         // here we subtarct the subarray with at most k odd number to the at most k-1 odd number
+//         return number(nums,k)-number(nums,k-1);
+//      }
+int numberOfSubarrays(vector<int>& nums, int k){
+    int n=nums.size();
+    int sum=0;
+    int cnt=0;
+    map<int,int>mpp;
+    mpp[0]=1;
+    for(int i=0;i<n;i++)
+    {
+        sum+=nums[i]%2;
+        int l=sum-k;
+        if(mpp.find(l)!=mpp.end())
         {
-          if(nums[j]%2!=0)
-          k--;
-         while(i<=j and k<0)
-         {
-            if(nums[i]%2!=0)
-            k++;
-            i++;
-         }
-         cnt+=j-i+1;
-         j++;
+            cnt+=mpp[l];
+        }
+        mpp[sum]++;
     }
     return cnt;
-    }
-     int numberOfSubarrays(vector<int>& nums, int k){
-        return number(nums,k)-number(nums,k-1);
-     }
+}
+
 };
