73852285056791b00e7fb9ee92f93e0342a090bb
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-02-02 12:27:53 +0530

    Time: 2 ms (59.99%) | Memory: 32.4 MB (97.40%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
+  int SubarraysWithSum(vector<int>& nums, int goal) {
         int n=nums.size();
-        map<int,int>mpp;
-        mpp[0]=1;
-        int sum=0;
+        int s=0;
+        int e=0;
         int cnt=0;
-        for(int i=0;i<n;i++)
+        int sum=0;
+        for(int e=0;e<n;e++)
         {
-            sum+=nums[i];
-            int k=sum-goal;
-            if(mpp.find(k)!=mpp.end())
+            sum+=nums[e];
+            while(s<=e and sum>goal)
             {
-                cnt+=mpp[k];
+                sum-=nums[s];
+                s++;
             }
-            mpp[sum]++;
+           cnt+=e-s+1;
         }
         return cnt;
     }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+         return SubarraysWithSum(nums,goal)-SubarraysWithSum(nums,goal-1);
+    }
+    
+    //     map<int,int>mpp;
+    //     mpp[0]=1;
+    //     int sum=0;
+    //     int cnt=0;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         sum+=nums[i];
+    //         int k=sum-goal;
+    //         if(mpp.find(k)!=mpp.end())
+    //         {
+    //             cnt+=mpp[k];
+    //         }
+    //         mpp[sum]++;
+    //     }
+    //     return cnt;
+    // }
 };
