0c23439fdbafab1aebe580d3a48758254d92ec51
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-02-17 14:20:07 +0530

    Time: 32 ms (42.38%) | Memory: 74.2 MB (27.03%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,26 +1,68 @@
 class Solution {
 public:
-    int maximumSum(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
-      int maxi=-1;
-      int n=nums.size();
-      map<int,int>mpp;
-      for(int i=0;i<n;i++)
-      {
-        int sum=0;
+    // int maximumSum(vector<int>& nums) {
+//         // time complexity=O(nlogn)
+//       sort(nums.begin(),nums.end());
+//       int maxi=-1;
+//       int n=nums.size();
+//       map<int,int>mpp;
+//     //   time for this loop O(n)
+//       for(int i=0;i<n;i++)
+//       {
+//         int sum=0;
+//         int n=nums[i];
+//         // time O(9)
+//         while(n!=0)
+//         {
+//             int r=n%10;
+//             sum+=r;
+//             n=n/10;
+//         }
+//         // O(logn)
+//         if(mpp.find(sum)!=mpp.end())
+//         {
+//             maxi=max(maxi,(nums[i]+mpp[sum]));
+//         }
+//         mpp[sum]=nums[i];
+//       }
+//       return maxi;
+//     }
+// };
+// time complexity O(nlogn)
+
+int maximumSum(vector<int>& nums) {
+    vector<priority_queue<int,vector<int>,greater<int>>>q(82);
+    int maxi=-1;
+    for(int i=0;i<nums.size();i++)
+    {
+         int sum=0;
         int n=nums[i];
+        // time O(9)
         while(n!=0)
         {
             int r=n%10;
             sum+=r;
             n=n/10;
         }
-        if(mpp.find(sum)!=mpp.end())
+        q[sum].push(nums[i]);
+        if(q[sum].size()>2)
+        q[sum].pop();
+    }
+    for(auto it:q)
+    {
+        if(it.size()==2)
         {
-            maxi=max(maxi,(nums[i]+mpp[sum]));
+            int first=it.top();
+            it.pop();
+            int sec=it.top();
+            it.pop();
+            maxi=max(maxi,first+sec);
         }
-        mpp[sum]=nums[i];
-      }
-      return maxi;
+    }
+    return maxi;
+
+
     }
 };
+
+
