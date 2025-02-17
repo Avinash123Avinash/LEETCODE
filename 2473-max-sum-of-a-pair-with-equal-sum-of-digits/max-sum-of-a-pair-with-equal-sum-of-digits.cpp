7701b544de393a76777bc9d78aae611ee47c90b7
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-02-17 14:27:19 +0530

    Time: 54 ms (22.70%) | Memory: 73.6 MB (27.05%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // int maximumSum(vector<int>& nums) {
+//     int maximumSum(vector<int>& nums) {
 //         // time complexity=O(nlogn)
 //       sort(nums.begin(),nums.end());
 //       int maxi=-1;
@@ -30,12 +30,51 @@ public:
 // };
 // time complexity O(nlogn)
 
-int maximumSum(vector<int>& nums) {
-    vector<priority_queue<int,vector<int>,greater<int>>>q(82);
-    int maxi=-1;
-    for(int i=0;i<nums.size();i++)
-    {
-         int sum=0;
+// int maximumSum(vector<int>& nums) {
+//     vector<priority_queue<int,vector<int>,greater<int>>>q(82);
+//     int maxi=-1;
+//     for(int i=0;i<nums.size();i++)
+//     {
+//          int sum=0;
+//         int n=nums[i];
+//         // time O(9)
+//         while(n!=0)
+//         {
+//             int r=n%10;
+//             sum+=r;
+//             n=n/10;
+//         }
+//         q[sum].push(nums[i]);
+//         if(q[sum].size()>2)
+//         q[sum].pop();
+//     }
+//     for(auto it:q)
+//     {
+//         if(it.size()==2)
+//         {
+//             int first=it.top();
+//             it.pop();
+//             int sec=it.top();
+//             it.pop();
+//             maxi=max(maxi,first+sec);
+//         }
+//     }
+//     return maxi;
+
+
+//     }
+// };
+
+    int maximumSum(vector<int>& nums) {
+        // time complexity=O(nlogn)
+      sort(nums.begin(),nums.end());
+      int maxi=-1;
+      int n=nums.size();
+      vector<int>mpp(82,0);
+    //   time for this loop O(n)
+      for(int i=0;i<n;i++)
+      {
+        int sum=0;
         int n=nums[i];
         // time O(9)
         while(n!=0)
@@ -44,24 +83,14 @@ int maximumSum(vector<int>& nums) {
             sum+=r;
             n=n/10;
         }
-        q[sum].push(nums[i]);
-        if(q[sum].size()>2)
-        q[sum].pop();
-    }
-    for(auto it:q)
-    {
-        if(it.size()==2)
+        // O(logn)
+        if(mpp[sum]!=0)
         {
-            int first=it.top();
-            it.pop();
-            int sec=it.top();
-            it.pop();
-            maxi=max(maxi,first+sec);
+            maxi=max(maxi,(nums[i]+mpp[sum]));
         }
-    }
-    return maxi;
-
-
+        mpp[sum]=max(mpp[sum],nums[i]);
+      }
+      return maxi;
     }
 };
 
