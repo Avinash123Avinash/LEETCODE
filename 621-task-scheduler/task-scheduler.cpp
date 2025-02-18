d95d363f73ba8201c73122c32ffd113281ebc7a1
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-02-18 16:12:58 +0530

    Time: 43 ms (25.12%) | Memory: 47.8 MB (19.79%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         int m=tasks.size();
+        // here we use freq array for storing the character
        int freq[26]={0};
        for(int i=0;i<m;i++)
        {
         freq[tasks[i]-'A']++;
        }
+    //    here we store most frquent element on the top
        priority_queue<int>pq;
        for(int i=0;i<26;i++)
        {
@@ -19,8 +21,10 @@ public:
        while(!pq.empty())
        {
         vector<int>temp;
+        // here we push the unique element in the temp for n group keliye
         for(int i=1;i<=n+1;i++)
         {
+            // here we reduces the freq of each elemnt by one because it utilizethe one element from it
             if(!pq.empty())
             {
                 int fre=pq.top();
@@ -29,7 +33,7 @@ public:
                 temp.push_back(fre);
             }
         }
-
+//  here we store the  fre of the element if frq is not zero
         for(auto it:temp)
         {
             if(it>0)
