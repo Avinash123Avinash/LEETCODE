729d04e9c3138b3fb90b1e379198564d8288998f
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-02-24 18:24:34 +0530

    Time: 3 ms (36.92%) | Memory: 9.2 MB (20.04%) - LeetSync

--- 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.cpp
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.cpp
@@ -2,32 +2,20 @@ class Solution {
 public:
     string findValidPair(string s) {
         int n = s.size();
-        map<int, int> mpp;
-        string ans;
-        vector<int> vis(10, 0);
+        map<char, int> mpp;
         for (int i = 0; i < n; i++) {
-            int j = s[i] - '0';
-            mpp[j]++;
+            mpp[s[i]]++;
         }
-        cout<<mpp[2]<<endl;
-        cout<<mpp[3]<<endl;
-        int k=0;
-        int l=1;
-        while (l < n) {
-            int f=s[k]-'0';
-            int j = s[l] - '0';
-            cout << j << endl;
-            if (mpp[f] == f and mpp[j] == j and  vis[j] == 0 and vis[f] == 0  and j!=f) {
-                
-                    ans.push_back(s[k]);
-                    ans.push_back(s[l]);
-                vis[j] = 1;
-                vis[f]=1;
-                break;
-            }
-            k=l;
-            l++;
+        string ans="";
+      for(int i=0;i<n;i++)
+      {
+        string t=s.substr(i,2);
+        if(mpp[t[0]]==t[0]-'0' and mpp[t[1]]==t[1]-'0' and t[0]!=t[1])
+        {
+            ans=t;
+            return ans;
         }
+      }
 
         return ans;
     }
