fc21301e422115ebae0368623e063fc321a17fed
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-03-18 01:19:40 +0530

    Time: 9 ms (51.43%) | Memory: 20.8 MB (86.86%) - LeetSync

--- 1647-can-convert-string-in-k-moves/can-convert-string-in-k-moves.cpp
+++ 1647-can-convert-string-in-k-moves/can-convert-string-in-k-moves.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     bool canConvertString(string s, string t, int k) {
+        // here we first make the visited array for trace the element;
         vector<int>vis(26,0);
         int n=s.size();
         if(s.size()!=t.size())
         return false;
         for(int i=0;i<n;i++)
         {
+            // find the total shist required and add 26 abd take modulo means if we gaet the 
+            // smaller elemnt in the in t then add more than 26 in the s to get the t
             int shift=(t[i]-s[i]+26)%26;
+            // if same elemnt encounter then continue 
             if(shift==0)
             continue;
+
             int time=vis[shift]*26+shift;
+             vis[shift]++;
             if(time>k)
             return false;
-             vis[shift]++;
+            
         }
         return true;
     }
