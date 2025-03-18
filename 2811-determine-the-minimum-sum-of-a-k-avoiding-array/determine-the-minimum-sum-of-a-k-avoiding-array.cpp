749e684e3d44703a92199e5d79bd52a1d0ddee61
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-03-18 22:35:38 +0530

    Time: 5 ms (63.35%) | Memory: 11.2 MB (65.22%) - LeetSync

--- 2811-determine-the-minimum-sum-of-a-k-avoiding-array/determine-the-minimum-sum-of-a-k-avoiding-array.cpp
+++ 2811-determine-the-minimum-sum-of-a-k-avoiding-array/determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -3,9 +3,9 @@ public:
     int minimumSum(int n, int k) {
         vector<int>vis(k+1,0);
         int sum=0;
-        int cnt=0;
+        int cnt=1;
         int i=1;
-        while(cnt<n)
+        while(cnt<=n)
         {
             if(k-i>=0 and vis[i]!=1)
             {
