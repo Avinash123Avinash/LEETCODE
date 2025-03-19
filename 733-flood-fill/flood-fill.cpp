55db16f935b76c81e0791ce3efaf1ea811d5f36f
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-03-20 01:50:36 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (76.28%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,27 +1,32 @@
 class Solution {
-    void dfs(vector<vector<int>>& image,vector<vector<int>>& ans,int initial,int ncol[],
-    int nrow[],int row,int col,int color)
-    {
-        int n=image.size();
-        int m=image[0].size();
-        ans[row][col]=color;
-        for(int i=0;i<4;i++)
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& arr, int sr, int sc, int color) {
+        int n=arr.size();
+        int m=arr[0].size();
+        vector<vector<int>>ans=arr;
+        if(arr[sr][sc]==color)
+        return ans;
+        ans[sr][sc]=color;
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        int original=arr[sr][sc];
+        int delrow[4] = {-1, 1, 0, 0};
+        int delcol[4] = {0, 0, -1, 1};
+        while(q.size()!=0)
         {
-            int delrow=row+nrow[i];
-          int delcol=col+ncol[i];
-            if(delrow>=0 and delrow<n and delcol>=0 and delcol<m and ans[delrow][delcol]==initial and ans[delrow][delcol]!=color)
+            auto node=q.front();
+            q.pop();
+            for(int k=0;k<4;k++)
             {
-                dfs(image,ans,initial,ncol,nrow,delrow,delcol,color);
+                int nr=node.first+delrow[k];
+                int nc=node.second+delcol[k];
+                if(nr>=0 and nr<n and nc>=0 and nc<m and ans[nr][nc]==original)
+                {
+                    ans[nr][nc]=color;
+                    q.push({nr,nc});
+                }
             }
         }
-    }
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int initial = image[sr][sc];
-        vector<vector<int>> ans=image;
-        int nrow[]={-1,0,+1,0};
-        int ncol[]={0,+1,0,-1};
-        dfs(image,ans,initial,ncol,nrow,sr,sc,color);
         return ans;
     }
 };
