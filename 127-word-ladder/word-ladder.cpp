57f6cd95dd9839c9e80f370879fac179d74a7b20
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-03-24 21:10:19 +0530

    Time: 39 ms (92.89%) | Memory: 21 MB (70.12%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int ladderLength(string begin, string end, vector<string>& arr) {
+        unordered_set<string>mpp(arr.begin(),arr.end());
+        int n=arr.size();
+        if(mpp.find(end)==mpp.end())
+        return 0;
+
+        queue<pair<string,int>>q;
+        q.push({begin,1});
+        mpp.erase(begin);
+        while(!q.empty() )
+        {
+            auto [word,step]=q.front();
+            q.pop();
+            if(word==end)
+            return step;
+            
+            for(int i=0;i<word.size();i++)
+            {
+                char org=word[i];
+                for( char ch='a';ch<='z';ch++)
+                {
+                     if(ch==org)
+                     continue;
+                     word[i]=ch;
+                     if(mpp.find(word)!=mpp.end())
+                     {
+                        q.push({word,step+1});
+                        mpp.erase(word);
+                     }
+                }
+                word[i]=org;
+            }
+        }
+return 0;
+    }
+};
