c22d35ddfa79eb3b89aae5830565d5afa22308fc
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-04-08 15:00:53 +0530

    Time: 11 ms (86.75%) | Memory: 38.9 MB (23.77%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,39 +3,37 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, 0));
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        vector<vector<int>> dis(n, vector<int>(m, 0));
         queue<pair<pair<int, int>, int>> q;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
-                    q.push({{i, j}, 0});
                     vis[i][j] = 1;
-                } else {
-                    vis[i][j] = 0;
+                    q.push({{i, j}, 0});
                 }
             }
         }
-        int delrow[4]={-1,0,+1,0};
-        int delcol[4]={0,+1,0,-1};
+        int delrow[4] = {-1, 1, 0, 0};
+        int delcol[4] = {0, 0, -1, 1};
         while(!q.empty())
         {
-            int r=q.front().first.first;
-            int c=q.front().first.second;
+            auto[r,c]=q.front().first;
             int d=q.front().second;
             q.pop();
+            ans[r][c]=d;
             for(int i=0;i<4;i++)
             {
-                int nrow=r+delrow[i];
-                int ncol=c+delcol[i];
-                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and vis[nrow][ncol]==0)
+                int nr=r+delrow[i];
+                int nc=c+delcol[i];
+                if(nr>=0 and nc>=0 and nr<n and nc<m and vis[nr][nc]!=1)
                 {
-                  vis[nrow][ncol]=1;
-                  dis[nrow][ncol]=d+1;
-                  q.push({{nrow,ncol},d+1});
+                    vis[nr][nc]=1;
+                    q.push({{nr,nc},1+d});
                 }
             }
         }
-        return dis;
+        return ans;
+
     }
 };
