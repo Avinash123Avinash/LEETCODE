4d7859f09168987cb9ea704307480cdacdca772b
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-05-17 17:03:44 +0530

    Time: 2 ms (18.71%) | Memory: 9.4 MB (33.01%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-int f( vector<vector<int>>&dp,int i,int j,int m,int n)
-{
-    if(i==0 and j==0)
-    return 1;
-    if(i<0 || j<0)
-    return 0;
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-    int up=f(dp,i-1,j,m,n);
-    int left=f(dp,i,j-1,m,n);
-    return dp[i][j]=up+left;
 
+int mod=2*1e9;
+int  f(int i,int j,int& cnt,int m,int n, vector<vector<int>>&dp)
+{
+    
+    if(i==m-1 and j==n-1)
+    {
+       return 1;
+    }
+     if(dp[i][j]!=-1) return dp[i][j];
+    int d=0;
+    int l=0;
+    if(i<m-1)
+     d=f(i+1,j,cnt,m,n,dp);
+    if(j<n-1)
+    l=f(i,j+1,cnt,m,n,dp);
+    return dp[i][j]=(l+d);
 }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        int i=m-1;
-        int j=n-1;
-        return f(dp,i,j,m,m);
+        int cnt=0;
+       return f(0,0,cnt,m,n,dp);
+        
     }
 };
