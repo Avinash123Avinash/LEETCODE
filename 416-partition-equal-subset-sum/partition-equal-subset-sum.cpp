261910023174c27e76857c2077ab8804cf62f668
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-05-27 12:45:41 +0530

    Time: 172 ms (20.13%) | Memory: 17.4 MB (47.57%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,17 +1,45 @@
 class Solution {
 public:
-bool f(int ind,int k,vector<int>&nums,vector<vector<int>>&dp)
-{
-    if(k==0) return true;
-    if(ind==0) return nums[0]==k;
-    if(dp[ind][k]!=-1)
-    return dp[ind][k];
-      bool taken =false;
+// this is the memoization technique
+// bool f(int ind,int k,vector<int>&nums,vector<vector<int>>&dp)
+// {
+//     if(k==0) return true;
+//     if(ind==0) return nums[0]==k;
+//     if(dp[ind][k]!=-1)
+//     return dp[ind][k];
+//       bool taken =false;
       
-    if(nums[ind]<=k)
-     taken = f(ind-1,k-nums[ind],nums,dp);
-     bool nottaken=f(ind-1,k,nums,dp);
-     return dp[ind][k]= taken||nottaken;
+//     if(nums[ind]<=k)
+//      taken = f(ind-1,k-nums[ind],nums,dp);
+//      bool nottaken=f(ind-1,k,nums,dp);
+//      return dp[ind][k]= taken||nottaken;
+// }
+bool f(int n,int sum,vector<int>&nums)
+{
+    vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
+    for(int i=0;i<=n;i++)
+    {
+        dp[i][0]=true;
+    }
+    if(nums[0]==sum)
+    dp[0][nums[0]]=true;
+    
+//   =false;
+    for(int i=1;i<n+1;i++)
+    {
+        bool taken=false;
+        for(int j=1;j<sum+1;j++)
+        {
+            if(nums[i-1]<=j)
+          taken =dp[i-1][j-nums[i-1]];
+             bool nottaken=dp[i-1][j];
+             dp[i][j]=taken||nottaken;
+        }
+      
+    }
+    return dp[n][sum];
+
+    
 }
     bool canPartition(vector<int>& nums) {
         int sum=0;
@@ -22,8 +50,7 @@ bool f(int ind,int k,vector<int>&nums,vector<vector<int>>&dp)
             sum+=nums[i];
         }
         int l=sum/2;
-        vector<vector<int>>dp(n+1,vector<int>(l+1,-1));
-      return sum%2!=0?false:f(n-1,sum/2,nums,dp);
+      return sum%2!=0?false:f(n,sum/2,nums);
         
     }
 };
