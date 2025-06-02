49686a742bafd7f488c6a0528ec0e3020ce5f165
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-06-03 01:36:39 +0530

    Time: 41 ms (43.86%) | Memory: 53.2 MB (11.43%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
+    int f(int ind, int k, vector<int>& coins,vector<vector<int>>&dp) {
+        if (ind == 0) {
+            if (k % coins[0] == 0)
+                return dp[ind][k]=k / coins[0];
+            return dp[ind][k]=1e9;
+        }
+        if(dp[ind][k]!=-1)
+        return dp[ind][k];
+
+        // if (k % coins[ind] == 0)
+        //     return k / coins[ind];
+        int taken = 1e9;
+        if (coins[ind] <= k) {
+            taken = 1 + f(ind, k - coins[ind], coins,dp);
+        }
+        int nottaken = f(ind - 1, k, coins,dp);
+        
+        return dp[ind][k]=min(taken, nottaken);
+    }
     int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX));
-
-        // Initialize the dp array
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 0; // Amount 0 can always be achieved with 0 coins
-        }
-
-        for (int j = 1; j <= amount; j++) {
-            if (j % coins[0] == 0)
-                dp[1][j] = j / coins[0];
-            else
-                dp[1][j] = INT_MAX - 1; // Set it to a large value
-        }
-
-        // Fill the dp table
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (coins[i - 1] <= j) {
-                    dp[i][j] = min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-
-        // If it's not possible to form the amount, return -1
-        return dp[n][amount] == INT_MAX-1 ? -1 : dp[n][amount];
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        f(n - 1, amount, coins,dp);
+        return dp[n-1][amount]!=1e9?dp[n-1][amount]:-1;
     }
 };
