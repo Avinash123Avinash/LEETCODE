9bd479eafab0be0795351613b7ce0694bcd74055
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-06-03 15:04:10 +0530

    Time: 20 ms (48.50%) | Memory: 28.5 MB (39.21%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
+int f(int ind,int k,vector<int>&coins,vector<vector<int>>&dp)
+{
+    if(ind==0)
+    {
+        if(k%coins[0]==0)
+        return dp[ind][k]=1;
+        return dp[ind][k]=0;
+    }
+    if(dp[ind][k]!=-1)
+    return dp[ind][k];
+    int take=0;
+    if(coins[ind]<=k)
+    take=f(ind,k-coins[ind],coins,dp);
+   int nottake=f(ind-1,k,coins,dp);
+    return dp[ind][k]=take+nottake;
+}
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        int dp[n+1][amount+1];
-        for(int i=0;i<n+1;i++)
-        {
-            for(int j=0;j<amount+1;j++)
-            {
-                if(i==0)
-                dp[i][j]=0;
-                if(j==0)
-                dp[i][j]=1;
-            }
-        }
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<amount+1;j++)
-            {
-                if(coins[i-1]<=j)
-                dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
-                else
-                dp[i][j]=dp[i-1][j];
-            }
-        }
-        return dp[n][amount];
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        f(n-1,amount,coins,dp);
+        return dp[n-1][amount];
     }
 };
