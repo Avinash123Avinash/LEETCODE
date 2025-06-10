a0170ae30d1a67576d077ccc21826943e2f87ead
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-06-10 15:57:23 +0530

    Time: 38 ms (25.45%) | Memory: 31.7 MB (16.06%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
- int lps(string& s,int n) {
-        // int n=s.size();
-  string b(s.rbegin(), s.rend()); 
-       int dp[n+1][n+1];
-       for(int i=0;i<n+1;i++)
-       {
-        for(int j=0;j<n+1;j++)
-        {
-            if(i==0 || j==0)
-            {
-                dp[i][j]=0;
-            }
-        }
-       }
-       for(int i=1;i<n+1;i++)
-       {
-        for(int j=1;j<n+1;j++)
-        {
-            if(s[i-1]==b[j-1])
-            dp[i][j]=1+dp[i-1][j-1];
-            else
-            {
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-       }
-       return dp[n][n];
-    }
     int minInsertions(string s) {
+        string s2=s;
+        reverse(s2.begin(),s2.end());
         int n=s.size();
-        int k=lps(s,n);
-return n-k;
+          vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                int match = 0;
+                int notmatch = max(dp[i - 1][j], dp[i][j - 1]);
+                if (s[i-1] == s2[j-1])
+                    match = 1 + dp[i - 1][j - 1];
+
+                dp[i][j] = max(match, notmatch);
+            }
+        }
+        return n-dp[n][n];
     }
 };
