c1920b2713493a255aec8072a6cf6213b38acbe2
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-06-10 15:59:47 +0530

    Time: 27 ms (71.69%) | Memory: 31.5 MB (58.90%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int minInsertions(string s) {
+        int n=s.size();
         string s2=s;
         reverse(s2.begin(),s2.end());
-        int n=s.size();
-          vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 0;
-        for (int j = 0; j <= n; j++) {
-            dp[0][j] = 0;
-        }
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-
-                int match = 0;
-                int notmatch = max(dp[i - 1][j], dp[i][j - 1]);
-                if (s[i-1] == s2[j-1])
-                    match = 1 + dp[i - 1][j - 1];
-
-                dp[i][j] = max(match, notmatch);
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                int notmatch=max(dp[i-1][j],dp[i][j-1]);
+                int match=0;
+                if(s[i-1]==s2[j-1])
+                {
+                  match=  1+dp[i-1][j-1];
+                }
+                dp[i][j]=max(match,notmatch);
             }
         }
         return n-dp[n][n];
