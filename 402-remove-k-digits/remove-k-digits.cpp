758abc6f4ebe88ec2285cadd17d4692a93cd3565
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-06-30 17:20:33 +0530

    Time: 11 ms (7.87%) | Memory: 11.4 MB (69.35%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n = num.size();
-        string ans;
-        if (n == k)
-            return "0";
-    for(char dig:num)
-    {
-        while(!ans.empty() and k>0 and ans.back()>dig)
+       int n=num.size();
+       stack<char>st;
+       for(int i=0;i<n;i++)
+       {
+        while(!st.empty() and k>0 and (st.top()-'0')>(num[i]-'0'))
+        {
+            k--;
+            st.pop();
+        }
+        st.push(num[i]);
+       }
+       string ans;
+       while(k>0)
+       {
+        st.pop();
+        k--;
+       }
+       while(!st.empty())
+       {
+         ans.push_back(st.top());
+         st.pop();
+        }
+        while(ans.size()!=0 and ans.back()=='0')
         {
             ans.pop_back();
-            k--;
         }
-        ans+=dig;
-    }
-    while(k>0)
-    {
-        ans.pop_back();
-        k--;
-    }
-    // for check the leading zero
-    int i=0;
-    while( i<ans.size() and ans[i]=='0')
-    {
-        i++;
-    }
-   ans=ans.substr(i);
-    return ans.empty()?"0":ans;
+       reverse(ans.begin(),ans.end());
+       if(ans.size()==0)
+         return "0";
+        return ans;
     }
 };
