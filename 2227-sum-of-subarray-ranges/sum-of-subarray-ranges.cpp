0fd7f278f6846039bd7844c6f55ae28f51b5fb65
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-06-30 15:30:25 +0530

    Time: 246 ms (5.05%) | Memory: 15.9 MB (28.18%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,21 +1,129 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n=nums.size();
-        long long sum=0;
-        int mini=INT_MAX;
-        int maxi=INT_MIN;
-        for(int i=0;i<n;i++)
+vector<int> findnse(vector<int>&arr)
+{
+    int n=arr.size();
+    vector<int>ans(n,n);
+    stack<int>st;
+    for(int i=0;i<n;i++)
+    {
+        while(!st.empty() and arr[st.top()]>arr[i])
         {
-            mini=INT_MAX;
-            maxi=INT_MIN;
-            for(int j=i;j<n;j++)
-            {
-                mini=min(mini,nums[j]);
-                maxi=max(maxi,nums[j]);
-                sum=sum+(maxi-mini);
-            }
+            ans[st.top()]=i;
+            st.pop();
         }
-        return sum;
+    st.push(i);
+    }
+     for(int i=0;i<n;i++)
+    {
+        cout<<ans[i]<<endl;
+    }
+
+    return ans;
+
+}
+vector<int>findpse(vector<int>&arr)
+{
+    int n=arr.size();
+    vector<int>ans(n,-1);
+    stack<int>st;
+      for(int i=n-1;i>=0;i--)
+    {
+       
+        while(!st.empty() and arr[st.top()]>=arr[i])
+        {
+            ans[st.top()]=i;
+            st.pop();
+        }
+    st.push(i);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<ans[i]<<endl;
+    }
+    return ans;
+
+}
+    long long findmin(vector<int>& arr) {
+        long long sum=0;
+        int n=arr.size();
+        vector<int>nse;
+        vector<int>pse;
+        nse=findnse(arr);
+        pse=findpse(arr);
+       for(int i=0;i<n;i++)
+       {
+        int r=nse[i]-i;
+        int l=i-pse[i];
+    sum=sum+(l*1l*r*1ll*arr[i]);
+       }
+       return sum;
+    }
+
+vector<int> findnge(vector<int>&arr)
+{
+    int n=arr.size();
+    vector<int>ans(n,n);
+    stack<int>st;
+    for(int i=0;i<n;i++)
+    {
+        while(!st.empty() and arr[st.top()]<arr[i])
+        {
+            ans[st.top()]=i;
+            st.pop();
+        }
+    st.push(i);
+    }
+     for(int i=0;i<n;i++)
+    {
+        cout<<ans[i]<<endl;
+    }
+
+    return ans;
+
+}
+
+vector<int>findpge(vector<int>&arr)
+{
+    int n=arr.size();
+    vector<int>ans(n,-1);
+    stack<int>st;
+      for(int i=n-1;i>=0;i--)
+    {
+       
+        while(!st.empty() and arr[st.top()]<=arr[i])
+        {
+            ans[st.top()]=i;
+            st.pop();
+        }
+    st.push(i);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<ans[i]<<endl;
+    }
+    return ans;
+
+}
+    long long findmax(vector<int>& arr) {
+        long long sum=0;
+        int n=arr.size();
+        vector<int>nge;
+        vector<int>pge;
+        nge=findnge(arr);
+        pge=findpge(arr);
+       for(int i=0;i<n;i++)
+       {
+        int r=nge[i]-i;
+        int l=i-pge[i];
+    sum=sum+(l*1ll*r*1ll*arr[i]);
+       }
+       return sum;
+    }
+
+
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+       return (findmax(nums)*1ll-findmin(nums)*1ll);
     }
 };
