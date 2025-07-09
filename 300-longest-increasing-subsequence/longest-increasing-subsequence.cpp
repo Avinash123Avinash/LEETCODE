7a1a76294a146b46332a473e2cdaf80a3ed114dc
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-07-09 15:26:55 +0530

    Time: 559 ms (16.81%) | Memory: 296.6 MB (12.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    // int f(vector<int>& nums, int ind, int prev, int n,
-    //       vector<vector<int>>& dp) {
+    int f(int ind, int prev, vector<int>& nums, int n,
+          vector<vector<int>>& dp) {
+        if (ind == n)
+            return 0;
+        if (prev!=-1 and dp[ind][prev] != -1)
+            return dp[ind][prev];
+        int take = 0;
+        if (prev == -1 || nums[ind] > nums[prev])
+            take = 1 + f(ind + 1, ind, nums, n, dp);
 
-    //     if (ind == n)
-    //         return 0;
-    //     if (dp[ind][prev + 1] != -1)
-    //         return dp[ind][prev + 1];
-    //     // not take
-    //     int len1 = 0 + f(nums, ind + 1, prev, n, dp);
-    //     int len2 = 0;
-    //     if (prev == -1 || nums[ind] > nums[prev]) {
-    //         len2 = (1 + f(nums, ind + 1, ind, n, dp));
-    //     }
-    //     return dp[ind][prev + 1] = max(len1, len2);
-    // }
+        int skip = f(ind + 1, prev, nums, n, dp);
+            if(prev!=-1)
+            dp[ind][prev] = max(take, skip);
+        return  max(take, skip);
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        // int len=0;
-        int maxi=INT_MIN;
-     vector<int>dp(n,1);
-     for(int ind=0;ind<n;ind++)
-     {
-        for(int prev=0;prev<ind;prev++)
-        {
-            if(nums[ind]>nums[prev])
-            dp[ind]=max(dp[prev]+1,dp[ind]);
-        }
-        maxi=max(maxi,dp[ind]);
-     }
-     return maxi;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return f(0, -1, nums, n, dp);
     }
 };
