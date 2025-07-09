c87c613ac68e6bbf1dad1fca90c0b1fff87d0bc8
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-07-09 16:03:42 +0530

    Time: 72 ms (56.28%) | Memory: 14.3 MB (63.90%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    int f(int ind, int prev, vector<int>& nums, int n,
-          vector<vector<int>>& dp) {
-        if (ind == n)
-            return 0;
-        if (prev!=-1 and dp[ind][prev] != -1)
-            return dp[ind][prev];
-        int take = 0;
-        if (prev == -1 || nums[ind] > nums[prev])
-            take = 1 + f(ind + 1, ind, nums, n, dp);
+    // int f(int ind, int prev, vector<int>& nums, int n,
+    //       vector<vector<int>>& dp) {
+    //     if (ind == n)
+    //         return 0;
+    //     if (prev!=-1 and dp[ind][prev] != -1)
+    //         return dp[ind][prev];
+    //     int take = 0;
+    //     if (prev == -1 || nums[ind] > nums[prev])
+    //         take = 1 + f(ind + 1, ind, nums, n, dp);
 
-        int skip = f(ind + 1, prev, nums, n, dp);
-            if(prev!=-1)
-            dp[ind][prev] = max(take, skip);
-        return  max(take, skip);
-    }
+    //     int skip = f(ind + 1, prev, nums, n, dp);
+    //         if(prev!=-1)
+    //         dp[ind][prev] = max(take, skip);
+    //     return  max(take, skip);
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return f(0, -1, nums, n, dp);
+        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+
+                if(nums[j]<nums[i])
+                dp[i]=max((1+dp[j]),dp[i]);
+            }
+        }
+        int maxi=0;
+       for(int i=0;i<n;i++)
+       {
+        maxi=max(maxi,dp[i]);
+       }
+       return maxi;
     }
 };
