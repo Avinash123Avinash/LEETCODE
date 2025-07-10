77a9af8c7e8dcbefec5fb85c137b1b8ef33eedf0
Avinash kishore <89377645+Avinash123Avinash@users.noreply.github.com>
2025-07-10 18:51:59 +0530

    Time: 136 ms (5.01%) | Memory: 161.9 MB (5.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-vector<string>ans;
-void generate(string& s,int open ,int closed)
-{
-    if(closed==0 and open==0)
-    {
-        ans.push_back(s);
-        return;
-    }
-    if(open>0)
-    {
-        s.push_back('(');
-        generate(s,open-1,closed);
-        s.pop_back();
-    }
-    if(closed>0)
-    {
-        if(open<closed)
-        {
-            s.push_back(')');
-            generate(s,open,closed-1);
-            s.pop_back();
+    bool isvalid(string& temp) {
+        stack<char> st;
+        for (int i = 0; i < temp.size(); i++) {
+
+            if (!st.empty() and st.top()=='(' and temp[i]==')')
+                st.pop();
+            else
+                st.push(temp[i]);
         }
+
+        return st.size() != 0 ? false : true;
     }
-}
+    void f(int n, string& temp, vector<string>& ans) {
+        if (temp.size() == 2 * n) {
+            if (isvalid(temp))
+                ans.push_back(temp);
+
+            return;
+        }
+
+        temp.push_back('(');
+        f(n, temp, ans);
+        temp.pop_back();
+
+        temp.push_back(')');
+        f(n, temp, ans);
+        temp.pop_back();
+    }
     vector<string> generateParenthesis(int n) {
-         string s;
-         int open=n;
-         int closed=n;
-         generate(s,open,closed);
-           return ans;
+        vector<string> ans;
+        string temp;
+        f(n, temp, ans);
+        return ans;
     }
 };
